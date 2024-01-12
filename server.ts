e5c40427446c489003de5f7e36b62c5ce2399593
bpontiz <bpontiz@gmail.com>
2024-01-12 17:45:24 -0300

    [reset] branch for no routing

--- server.ts
+++ server.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import { FrontRouter } from './services/routes/adapters/drivers/front-router';
-// import { connectionConfig } from './services/repository/app/config';
 
 const app = express();
 
@@ -9,8 +7,6 @@ app.use(express.json());
 const PORT = 8080;
 
 const URL = `http://localhost:${PORT}`;
-
-app.use('/', new FrontRouter().appRouter);
 
 app.listen(PORT, () => {
     console.log(`✔ Server is running on ${URL}`);
